f550e906acf08f823fae4b63184f49ea70301d3e
UdohLawrence <lawrenceaudoh@gmail.com>
2024-05-03 05:41:26 +0100

    final commit

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,40 @@
+<script setup>
+import { ref } from "vue";
+
+const count = ref(0);
+</script>
+
+
 <template>
-  <div>
-    <h1>VueJS Counter App</h1>
-    <h4>The current count is....</h4>
-    <h2>0</h2>
-    <button>-</button>
-    <button>+</button>
-  </div>
+  <main>
+    <div>
+      <h1>VueJS Counter App</h1>
+      <h4>The current count is....</h4>
+      <h2>{{ count }}</h2>
+      <button @click="count--">-</button>
+      <button @click="count++">+</button>
+    </div>
+  </main>
 </template>
+
+<style scoped>
+main {
+  background-color: aliceblue;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+}
+button {
+  padding: 10px;
+  border-radius: 15px;
+  width: 30%;
+  background-color: aquamarine;
+  margin-top: 20px;
+  cursor: pointer;
+  font-size: 2rem;
+  margin: 8px;
+}
+</style>
